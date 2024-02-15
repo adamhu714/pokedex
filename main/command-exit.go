50cf5ca318785aa666fa35842a00717433a94901
adamhu714 <adamhu714@gmail.com>
2024-02-15 17:24:05 +0000

    Started REPL program.

---
+++ main/command-exit.go
@@ -0,0 +1,6 @@
+package main
+
+func commandExit() error {
+	// avoiding using os.exit(1)
+	return nil
+}
---
+++ main/repl.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("pokedexCLI >")
+		reader.Scan()
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			invalidCommand()
+			continue
+		}
+		commandName := words[0]
+		command, exists := getCommands()[commandName]
+		if !exists {
+			invalidCommand()
+			continue
+		}
+		if command.name == "exit" {
+			break
+		}
+		err := command.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func invalidCommand() {
+	fmt.Println("Please input a valid command.")
+}
+
+func cleanInput(text string) []string {
+	output := strings.TrimSpace(text)
+	output = strings.ToLower(output)
+	words := strings.Fields(output)
+	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exits the program.",
+			callback:    commandExit,
+		},
+	}
+}
