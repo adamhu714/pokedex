97901006d35d0b317fe442513398096cc06d5dc3
adamhu714 <adamhu714@gmail.com>
2024-02-16 22:17:41 +0000

    Added test function for cleanInput

---
+++ main/repl_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  ",
+			expected: []string{},
+		},
+		{
+			input:    " hello ",
+			expected: []string{"hello"},
+		},
+		{
+			input:    " hello world ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "  Hello World  ",
+			expected: []string{"hello", "world"},
+		},
+	}
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("lengths do not match: actual - '%v' vs expected - '%v'", actual, c.expected)
+			continue
+		}
+		for i := range actual {
+			if actual[i] != c.expected[i] {
+				t.Errorf("words %d do not match: actual - '%v' vs expected - '%v'", i, actual[i], c.expected[i])
+			}
+		}
+	}
+}
