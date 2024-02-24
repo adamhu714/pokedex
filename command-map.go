d1881ed1bb4b97dcf289b616d08adfa4a31a8172
adamhu714 <adamhu714@gmail.com>
2024-02-24 23:50:51 +0000

    Added boilerplate for mapb command

--- command-map.go
+++ command-map.go
@@ -23,5 +23,16 @@ func commandMapF() error {
 }
 
 func commandMapB() error {
+	pokeapiClient := pokeapi.NewClient()
+
+	resp, err := pokeapiClient.ListLocationAreas()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Location areas:")
+	for _, area := range resp.Results {
+		fmt.Printf("%s\n", area.Name)
+	}
+
 	return nil
 }
