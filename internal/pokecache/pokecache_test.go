c844d856d80bac4097b8ce3cf5cbbb434d32eedc
adamhu714 <adamhu714@gmail.com>
2024-02-28 23:24:35 +0000

    Added cache functions

--- internal/pokecache/pokecache_test.go
+++ internal/pokecache/pokecache_test.go
@@ -1,23 +1,71 @@
 package pokecache
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestCreateCache(t *testing.T) {
-	cache := NewCache()
+	cache := NewCache(time.Millisecond)
 	if cache.cache == nil {
 		t.Error("cache is nil")
 	}
 }
 
 func TestAddGetCache(t *testing.T) {
-	cache := NewCache()
+	cache := NewCache(time.Millisecond)
 
-	cache.Add("key1", []byte("val1"))
-	actual, ok := cache.Get("key1")
-	if !ok {
-		t.Error("key1 not found")
+	cases := []struct {
+		inputKey string
+		inputVal []byte
+	}{
+		{
+			inputKey: "key1",
+			inputVal: []byte("val1"),
+		},
+		{
+			inputKey: "key2",
+			inputVal: []byte("val2"),
+		},
+		{
+			inputKey: "",
+			inputVal: []byte("val3"),
+		},
 	}
-	if string(actual) != "val1" {
-		t.Error("value doesn't match")
+
+	for _, cas := range cases {
+		cache.Add(cas.inputKey, cas.inputVal)
+		actual, ok := cache.Get(cas.inputKey)
+		if !ok {
+			t.Errorf("%s not found", cas.inputKey)
+		}
+		if string(actual) != string(cas.inputVal) {
+			t.Errorf("%s doesn't match %s", string(actual),
+				cas.inputVal)
+		}
+	}
+
+}
+
+func TestReap(t *testing.T) {
+	interval := time.Millisecond * 10
+	cache := NewCache(interval)
+
+	keyOne := "key1"
+	cache.Add(keyOne, []byte("val1"))
+
+	time.Sleep(interval + 10*time.Millisecond)
+
+	if _, ok := cache.Get("key1"); ok {
+		t.Errorf("%s should have been reaped", keyOne)
+	}
+
+	keyTwo := "key2"
+	cache.Add(keyTwo, []byte("val2"))
+
+	time.Sleep(time.Millisecond)
+
+	if _, ok := cache.Get("key2"); !ok {
+		t.Errorf("%s should not have been reaped", keyTwo)
 	}
 }
