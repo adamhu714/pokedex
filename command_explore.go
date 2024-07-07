686b7140c1a35fd9773315aa7556e933e0b25fcf
adamhu714 <adamhu714@gmail.com>
2024-07-07 16:36:52 +0100

    Updated explore command to return id

--- command_explore.go
+++ command_explore.go
@@ -23,5 +23,6 @@ func callbackExplore(cfg *config, args ...string) error {
 	for _, encounter := range locationArea.PokemonEncounters {
 		fmt.Printf(" - %s\n", encounter.Pokemon.Name)
 	}
+	fmt.Println()
 	return nil
 }
